5c561646b14303ac5b5409df3ddf2b418d0dfecc
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2018-12-22 01:12:04 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ const brain = require('brain.js')
 const cancerData = require('./cancerData.json')
 
 let net = new brain.NeuralNetwork(); 
-let riskFactorArray = []; 0 
+let riskFactorArray = []; 
  
 for (let i = 1; i < cancerData.length; i++) {
 
     riskFactorArray.push({  
     
         input: { 
-            yrsSmoking: cancerData[i].yearsSmoking / 10,
+            //ag: cancerData[i].age / 100,
+            yrsSmoking: cancerData[i].yearsSmoking / 100,
         },
         output: {
             //yrsSmoking: cancerData[i].yearsSmoking ,
-            ag: cancerData[i].age / 10,
-            ctrsPY: cancerData[i].contrasPerYear / 10,
-            cncr: cancerData[i].cancer / 10,
+            ag: cancerData[i].age / 100,
+            yc: cancerData[i].yearsContracep / 100,
+            cncr: cancerData[i].cancer / 100,
         }
     })
 }
@@ -24,13 +25,6 @@ net.train(riskFactorArray, {
     iterations: 5000, // the maximum times to iterate the training data --> number greater than 0
     errorThresh: 0.0005, // the acceptable error percentage from training data --> number between 0 and 1
     log: true, // true to use console.log, when a function is supplied it is used --> Either true or a function
-    logPeriod: 10, // iterations between logging out --> number greater than 0
-    learningRate: 0.3, // scales with delta to effect training rate --> number between 0 and 1
-    momentum: 0.1, // scales with next layer's change value --> number between 0 and 1
-    callback: null, // a periodic call back that can be triggered while training --> null or function
-    callbackPeriod: 10, // the number of iterations through the training data between callback calls --> number greater than 0
-    timeout: Infinity // the max number of milliseconds to train for --> number greater than 0});
-
 });
 
 
@@ -38,7 +32,7 @@ let result = net.run({
     //age: 30,
     yrsSmoking: 0,
     //cncr: 0.0,
-    // ctrsPY: 0.9,
+    // yc: 0.0,
 });
 
 console.log(result)
